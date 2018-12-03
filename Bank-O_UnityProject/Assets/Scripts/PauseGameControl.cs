7b1919b365676557f2e1f79c0f7153686052713c
JoeSB <joe.sb.uc@gmail.com>
2018-12-03 02:28:05 -0600

    Menu de pausa funcionando

--- Bank-O_UnityProject/Assets/Scripts/PauseGameControl.cs
+++ Bank-O_UnityProject/Assets/Scripts/PauseGameControl.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseGameControl : MonoBehaviour {
+    public GameObject menuPausa;
 
 	// Use this for initialization
 	void Start () {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        menuPausa.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.P))
+		if(Input.GetKeyDown(KeyCode.Escape))
         {
             ShowCursor();
+            menuPausa.SetActive(!menuPausa.activeInHierarchy);
+        }
+
+        if(menuPausa.activeInHierarchy)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
 	}
 
@@ -31,5 +40,12 @@ public class PauseGameControl : MonoBehaviour {
             Cursor.visible = true;
         }
         
+    }
+
+    public void Continue()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        menuPausa.SetActive(false);
     }
 }
