324af0990e8a06311b46b0ae0f5267b56f429f6a
JoeSB <joe.sb.uc@gmail.com>
2018-12-03 23:35:51 -0600

    cantidad dinero primera version

--- Bank-O_UnityProject/Assets/Scripts/TakeObject.cs
+++ Bank-O_UnityProject/Assets/Scripts/TakeObject.cs
@@ -13,22 +13,42 @@ public class TakeObject : MonoBehaviour {
     public static int varo;
     public AudioSource sonido;
 
+    public GameObject MoneyValue;
+    public Text moneyText;
+
     public GameObject textFullMoney;
     private bool Dinerolleno;
 
 	// Use this for initialization
 	void Start () {
         varo = 0;
-	}
+
+        MoneyValue.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if(InTrigger)
         {
-            if(Input.GetMouseButtonDown(0))
+            if (objectTrigger.tag == tagBilletes)
+            {
+                MoneyValue.SetActive(true);
+                moneyText.text = "$1000";
+            }
+            else if (objectTrigger.tag == tagOro)
+            {
+                MoneyValue.SetActive(true);
+                moneyText.text = "$10000";
+            }
+            if (Input.GetMouseButtonDown(0))
             {
                 CatchMoney();
             }
+        }
+        else
+        {
+            MoneyValue.SetActive(false);
+            moneyText.text = "$xxx";
         }
         MaxMoney();
         textoVaro.text = "$" + varo.ToString();
@@ -39,7 +59,8 @@ public class TakeObject : MonoBehaviour {
         if(Dinerolleno == true)
             if(objectTrigger.tag == tagBilletes)
             {
-
+                MoneyValue.SetActive(true);
+                moneyText.text = "$1000";
                 Debug.Log("Agarraste billullo");
                 sonido.Play();
                 varo += 1000;
@@ -47,6 +68,8 @@ public class TakeObject : MonoBehaviour {
                 Destroy(objectTrigger.transform.parent.gameObject);
             }else if(objectTrigger.tag == tagOro)
             {
+                MoneyValue.SetActive(true);
+                moneyText.text = "$10000";
                 Debug.Log("Agarraste oro");
                 varo += 10000;
                 sonido.Play();
@@ -84,6 +107,7 @@ public class TakeObject : MonoBehaviour {
 
     private void OnTriggerStay(Collider other)
     {
+        InTrigger = true;
         objectTrigger = other.gameObject;
     }
 
